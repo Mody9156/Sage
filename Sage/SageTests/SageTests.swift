20d9d37038baa418a2f262c79ab0b154dd7f6f49
KEITA Modibo <Modykeita2@gmail.com>
2025-08-07 16:42:39 +0200

    nouveau projet

---
+++ Sage/SageTests/SageTests.swift
@@ -0,0 +1,17 @@
+//
+//  SageTests.swift
+//  SageTests
+//
+//  Created by Modibo on 07/08/2025.
+//
+
+import Testing
+@testable import Sage
+
+struct SageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
