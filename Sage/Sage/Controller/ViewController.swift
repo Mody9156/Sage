1af8cba72a413cbe0664556b0d8a714aed4da344
KEITA Modibo <Modykeita2@gmail.com>
2025-08-08 22:30:17 +0200

    mise en place des de la couleur

--- Sage/Sage/Controller/ViewController.swift
+++ Sage/Sage/Controller/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var updateLabel: UILabel!
+    
+    @IBOutlet weak var setButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+   func setUpdateLabel() {
+       let randomName : [String] = ["Larry","James","Magic","Kyrie","Carmelo"]
+       updateLabel.text = randomName.randomElement()
+    }
 
 }
 
