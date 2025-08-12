cd152e2f3ae34ebc9a9d33b99ca72c2a3bccc674
KEITA Modibo <Modykeita2@gmail.com>
2025-08-12 23:54:31 +0200

    modification de la function

--- Sage/Sage/Model/CallQuoteWithHTTPClient.swift
+++ Sage/Sage/Model/CallQuoteWithHTTPClient.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class CallQuoteWithHTTPClient {
+    let session : BasicHTTPClient
     
+    init(session: BasicHTTPClient) {
+        self.session = session
+    }
+    
+    func callQuote() async throws -> URLRequest {
+        let url = URL(string: "https://zenquotes.io/api/quotes")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        return request
+    }
+    
+    func sendInformation() async throws -> [Quote] {
+        let request = try await callQuote()
+        let (data,response) = try await session.sendQuote(request: request)
+        
+        guard let HTTPResponse =  response as? HTTPURLResponse, HTTPResponse.statusCode == 200 else {return []}
+        let encode = try JSONDecoder().decode([Quote].self, from: data)
+        print("encode: \(encode)")
+        return encode
+    }
 }
