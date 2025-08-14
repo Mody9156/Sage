0578b616adc3994b47e85dd88ce33f5d94e99381
KEITA Modibo <Modykeita2@gmail.com>
2025-08-14 18:01:02 +0200

    modification de la function

--- Sage/Sage/Controller/ViewController.swift
+++ Sage/Sage/Controller/ViewController.swift
@@ -11,27 +11,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var updateLabel: UILabel!
     let quote = CallQuoteWithHTTPClient()
     @IBOutlet weak var setButton: UIButton!
+    @IBOutlet weak var updateLabelForShowName: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
     func setUpdateLabel() async throws {
       
-        
         do{
             let quoteNa =  try await quote.sendInformation()
             for i in quoteNa {
                 updateLabel.text = i.name
+                updateLabelForShowName.text = i.text
             }
             
         }catch{
             print("error : \(error.localizedDescription)")
         }
-        
-      
     }
 
     @IBAction func setUpName(_ sender: Any) {
