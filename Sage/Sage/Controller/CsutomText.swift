5d62b89df65f3a6ce7aac4e10ff39da586d3f678
KEITA Modibo <Modykeita2@gmail.com>
2025-08-17 15:41:27 +0200

    nouveau fichier

---
+++ Sage/Sage/Controller/CsutomText.swift
@@ -0,0 +1,13 @@
+//
+//  CsutomText.swift
+//  Sage
+//
+//  Created by Modibo on 17/08/2025.
+//
+
+import Foundation
+
+
+class CustomText  {
+    
+}
--- Sage/Sage/Controller/ViewController.swift
+++ Sage/Sage/Controller/ViewController.swift
@@ -33,7 +33,8 @@ class ViewController: UIViewController {
               }
           }
     }
-
+    
+    
     func setUpdateLabel() async throws {
         
         do{
@@ -61,5 +62,8 @@ class ViewController: UIViewController {
      
         print("arrayRandomNumberToValues:\(array.count)")
     }
+    
+    t
+    
 }
 
